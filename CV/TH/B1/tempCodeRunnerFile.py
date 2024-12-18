125ddd30b63d52abee930324764d179f215ffa12
Tran Quang Ha <114595165+haqt2003@users.noreply.github.com>
2024-12-18 22:24:22 +0700

    Delete CV directory

--- CV/TH/B1/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-# Ảnh tĩnh
-image = cv2.imread('D:\\PTIT\\CV\\TH\\B1\\anh1.jpg') # Đọc ảnh
-cv2.imshow('Anh', image) # Hiển thị ảnh
-cv2.imwrite('D:\\PTIT\\CV\\TH\\B1\\anh_sau_ghi.jpg', image) # Ghi ảnh
-cv2.waitKey(0) 
-cv2.destroyAllWindows()
